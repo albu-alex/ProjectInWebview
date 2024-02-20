53aaa7dc566f420c6aff2e34af1adcfef7aa2db9
Alex Albu <alex.albu@tss-yonder.com>
2024-02-20 10:47:49 +0200

    Creation of a mock graph, suggestive UI and integration in react-native-webview

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Chart from './app/chart';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Chart />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
